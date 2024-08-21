ea9dcf8dae22c67a50c1369daf6dcdf204cbe23d
HomeArchbishop <1984096954@qq.com>
2024-08-21 14:46:12 +0800

    feat: add Graph data structure and its test

---
+++ graph/graph.go
@@ -0,0 +1,30 @@
+package graph
+
+type Graph struct {
+	edges    map[int]map[int]int
+	Directed bool
+}
+
+func (g *Graph) AddVertex(v int) {
+	if g.edges == nil {
+		g.edges = make(map[int]map[int]int)
+	}
+
+	if _, ok := g.edges[v]; !ok {
+		g.edges[v] = make(map[int]int)
+	}
+}
+
+func (g *Graph) AddEdge(one, two int) {
+	g.AddWeightedEdge(one, two, 0)
+}
+
+func (g *Graph) AddWeightedEdge(one, two, weight int) {
+	g.AddVertex(one)
+	g.AddVertex(two)
+
+	g.edges[one][two] = weight
+	if !g.Directed {
+		g.edges[two][one] = weight
+	}
+}
